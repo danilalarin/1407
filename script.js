cd2a66fc9b245a752e8350dc0981313f05a6221e
danilalarin <tudanila1@gmail.com>
2025-07-20 16:30:10 +0300

    commit pre-final  Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const startButton = document.getElementById("start-btn");
 const questionText = document.getElementById("question-text");
 const answersContainer = document.getElementById("answers-container");
 const currentQuestionSpan = document.getElementById("current-question");
-const totalQuestionSpan = document.getElementById("total-questions");
+const totalQuestionsSpan = document.getElementById("total-questions");
 const scoreSpan = document.getElementById("score");
 const finalScoreSpan = document.getElementById("final-score");
 const maxScoreSpan = document.getElementById("max-score");
@@ -46,3 +46,76 @@ const quizQuestions=[
         {text: "Au", correct: true},
         {text: "Aa", correct: false},],},];
 // QUIZ STATE VARS
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+totalQuestionsSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+// event listeners
+startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+function startQuiz(){
+    // reset vars
+    currentQuestionIndex = 0;
+    score = 0;
+    scoreSpan.textContent = 0;
+    startScreen.classList.remove("active");
+    quizScreen.classList.add("active");
+    showQuestion()}
+function showQuestion(){
+// reset state
+    answersDisabled = false;
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+    currentQuestionSpan.textContent = currentQuestionIndex + 1;
+    const progressPercent = (currentQuestionIndex / quizQuestions.length) * 100;
+    progressBar.style.width = progressPercent + "%";
+    questionText.textContent = currentQuestion.question;
+    answersContainer.innerHTML = "";
+    currentQuestion.answers.forEach((answer) => { 
+         const button = document.createElement("button");
+         button.textContent = answer.text;
+         button.classList.add("answer-btn");
+         button.dataset.correct = answer.correct;
+         button.addEventListener("click",selectAnswer);
+         answersContainer.appendChild(button);});}
+function selectAnswer(event){
+    // optimization check
+    if(answersDisabled) return; 
+    answersDisabled = true;
+    const selectedButton = event.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    /* Here Array.from() is used to convert the NodeList returned by
+    answersContainer.children into an Array, this is  because the NodeList is
+    not an array and we need to use the forEach method*/
+    Array.from(answersContainer.children).forEach(button =>{
+        if(button.dataset.correct ==="true"){
+            button.classList.add("correct");}else if(button === selectedButton){
+            button.classList.add("incorrect");}})
+if(isCorrect){
+    score++;
+    scoreSpan.textContent = score;}
+setTimeout(()=>{
+    currentQuestionIndex++
+    if(currentQuestionIndex < quizQuestions.length){
+        showQuestion();}else{showResults();}
+},1000)}
+function showResults(){
+    quizScreen.classList.remove("active");
+    resultScreen.classList.add("active");
+    finalScoreSpan.textContent = score;
+    const percentage = (score/quizQuestions.length)*100
+    if(percentage === 100)
+        {resultMessage.textContent="Perfect! You're a genius!";}
+        else if (percentage >= 80)
+        {resultMessage.textContent="Great job! You know your stuff!";}
+        else if (percentage >= 60)
+        {resultMessage.textContent="Good effort! Keep learning!";}
+        else if (percentage >= 40)
+        {resultMessage.textContent="Not bad! Try again to improve!";}
+        else
+        {resultMessage.textContent = "Keep studying! You'll get better!";}}
+function restartQuiz(){
+    resultScreen.classList.remove("active");
+
+    startQuiz();
+}
